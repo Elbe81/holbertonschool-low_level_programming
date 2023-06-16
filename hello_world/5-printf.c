1f7fb1716d09f9038028c11a92f01356a080e273
Elbe81 <luiselberivera@gmail.com>
2023-06-16 18:28:56 +0000

    Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5 - printf.c
+ * Programmer Luis Rivera
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," folloed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	printf(with proper grammar, but the out come is a piece of art,\n");
+
+	return (0);
+}
