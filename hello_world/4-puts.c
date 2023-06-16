5304955ddbe5c52462e52299e9b2e8ca06461f8c
Elbe81 <luiselberivera@gmail.com>
2023-06-16 16:38:01 +0000

    Puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * Programmer Luis Rivera
+ * 6-16-23.
+ * 4-puts.c_file
+ * Code that uses the function puts 
+ * to printout but not using printf.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
