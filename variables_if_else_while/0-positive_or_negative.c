8bac3ec273f9fe81e09259939f36ac7efa187b0b
Elbe81 <luiselberivera@gmail.com>
2023-06-19 18:54:18 +0000

    Ran Num

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,9 +7,10 @@
 #include <time.h>
 
 /**
- * main-positive_or_negative function
+ * main - function to return value of positive or
+ * negative number.
  *
- * Return; always 0
+ * Return; always 0.
  */
 int main(void)
 {
@@ -17,18 +18,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
-	{
+	 {
        printf("%d is positive\n", n);
-    }
+     }
    	else if (n < 0)
-   	{
+   	 {
         printf("%d is negative\n", n);
-    }
+     }
 	else
-	{
+	 {
         printf("%d is zero\n", n);
-    }
-	return (0);
+     }
+return (0);
 }
