8533e082fa6d4c734657bd0ec5b652ac95ca8895
Elbe81 <luiselberivera@gmail.com>
2023-06-20 17:42:40 +0000

    Combination Num

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,11 +13,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++)
+	for (number = 0; number <= 10; number++)
 	{
 		putchar('0' + number);
 
-		if (number != 9)
+		if (number != 10)
 		{
 			putchar(',');
 			putchar(' ');
