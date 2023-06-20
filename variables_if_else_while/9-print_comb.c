8fffc7b7f9281b05df8d7291d6adf983e49b4c88
Elbe81 <luiselberivera@gmail.com>
2023-06-20 17:32:25 +0000

    Combination Num

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+/*
+ * File 9-Print Comb
+ * Programmer Luis Rivera
+ */
+#include <stdio.h>
+/**
+ * main - This program prints all possible combinaton
+ * of numbers
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar('0' + number);
+
+		if (number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
