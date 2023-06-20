8093cd247cba875b14f4a16b9c46280f0e425794
Elbe81 <luiselberivera@gmail.com>
2023-06-20 15:05:58 +0000

    Print Alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * File 2-Print alphabet
+ * Programmer Luis Rivera
+ */
+#include <stdio.h>
+/**
+ * main - It is use to print the alphabet
+ * using put but not printf
+ *
+ * Return: 0
+ * */
+int main()
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
