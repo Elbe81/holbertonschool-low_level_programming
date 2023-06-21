65ff4c79791918c8c5d4bd930f2ca76a66b1340f
Elbe81 <luiselberivera@gmail.com>
2023-06-21 13:58:29 +0000

    Putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/*
+ *File 0-Putchar
+ *Programmer Luis Rivera
+ */
+#include <stdio.h>
+/**
+ * main- This code only prints "putchar"
+ *
+ * Return: 0
+ **/
+int main(void) 
+{
+	printf("_putchar\n");
+
+	return (0);
+}
