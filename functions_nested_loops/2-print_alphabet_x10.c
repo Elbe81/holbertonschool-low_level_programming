cd9211975954ce82fa66fb844b3c9a15fe880406
Elbe81 <luiselberivera@gmail.com>
2023-06-22 14:09:25 +0000

    Alphabe x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,21 @@
 /**
  * print_alphabet_x10 - Displays alphabet
  * ten times
- *
  * Return: 0
  **/
 void print_alphabet_x10(void)
 {
-	char letters = 'a';
+	char alphabet = 'a';
 	int counter = 0;
 
 	while (counter < 0)
 	{
-
-		while (letters < 123)
+		while (alphabet < 123)
 		{
-			_putchar(letters);
-			letters++;
+			_putchar(alphabet);
+			alphabet++;
 		}
-        letters = 'a';
+		alphabet = 'a';
 		_putchar(10);
 		counter++;
 	}
