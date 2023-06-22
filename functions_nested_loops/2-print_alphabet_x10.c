4cfbac79ba996845c9e2116885bc3ca445c0a255
Elbe81 <luiselberivera@gmail.com>
2023-06-22 14:48:15 +0000

    CV

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- * Luis
  * print_alphabet_x10 - prints the lowercase alphabet x10 with _putchar.
+ * Auth:Luis Rivera
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-  char alphabet = 'a';
-  int counter = 0;
+	char alphabet = 'a';
+	int counter = 0;
 
-  while (counter < 10)
-  {
-    while (alphabet < 123)
-    {
-      _putchar(alphabet);
-      alphabet++;
-    }
-  alphabet = 'a';
-  _putchar(10);
-  counter++;
-  }
+	while (counter < 10)
+	{
+		while (alphabet < 123)
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+	alphabet = 'a';
+	_putchar(10);
+	counter++;
+	}
 }
+
