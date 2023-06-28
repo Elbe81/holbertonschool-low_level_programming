56684d4344f94ca7c2a2935e2d3a30ba94b4aee2
Elbe81 <luiselberivera@gmail.com>
2023-06-28 15:03:05 +0000

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,20 +11,15 @@
 
 void print_array(int *a, int n)
 {
-	if (a == NULL)
-	{
-		return;
-	}
+    int i;
 
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d", a[i]);
-
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
-	}
-
-	printf();
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+        if (i < (n - 1))
+        {
+            printf(", ");
+        }
+    }
+    printf("\n");
 }
