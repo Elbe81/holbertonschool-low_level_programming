b2bb04c89dfd6465bd8ef9c9645fc4a205b15ade
Elbe81 <luiselberivera@gmail.com>
2023-06-29 13:57:50 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *_strncat - Fuction to concatenate two string.
+ *@dest: It is a pointer.
+ *@src: It is a pointer.
+ *
+ *Return: to string dest.
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_start = dest;
+
+
+	while (*dest != '\0')
+
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && n > 0)
+
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	*dest = '\0';
+
+	return dest_start;
+}
