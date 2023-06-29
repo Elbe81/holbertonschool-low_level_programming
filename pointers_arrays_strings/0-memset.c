b05c7d22991d1502be1ca24b1fc48e46007e8c6b
Elbe81 <luiselberivera@gmail.com>
2023-06-29 18:59:24 +0000

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,13 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	unsigned int i = 0;
 
-	while (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		*ptr = b;
-		ptr++;
-		n--;
+		*(s + i) = b;
 	}
 
 	return (s);
