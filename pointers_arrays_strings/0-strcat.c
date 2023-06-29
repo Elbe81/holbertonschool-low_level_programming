fe319d619a92eb226dbcb2247e851ae022e503ba
Elbe81 <luiselberivera@gmail.com>
2023-06-29 14:48:56 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    char *dest_ptr = dest;
+	char *dest_ptr = dest;
 
-    while (*dest_ptr != '\0')
-    {
-        dest_ptr++;
-    }
+	while (*dest_ptr != '\0')
+	{
+		dest_ptr++;
+	}
 
-    while (*src != '\0')
-    {
-        *dest_ptr = *src;
-        dest_ptr++;
-        src++;
-    }
+	while (*src != '\0')
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++;
+	}
 
-    *dest_ptr = '\0';
+	*dest_ptr = '\0';
 
-    return dest;
+	return (dest);
 }
 
