9b70c13469d053a3fc99e852b0d65d64f0ad63c4
Elbe81 <luiselberivera@gmail.com>
2023-06-30 02:13:58 +0000

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * memset - Function to fill memory with a constant.
+ * _memset - A function to fill memory with a constant.
  * @s: Pointer.
  * @b: Variable parameter
  * @n: Unsinged variable
