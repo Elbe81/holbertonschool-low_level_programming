dba80e84036e8424953d44a5ce7180d4ed9488e0
Elbe81 <luiselberivera@gmail.com>
2023-07-03 18:20:52 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -30,7 +30,16 @@ int _sqrt_helper(int n, int start, int end)
 	}
 	else if (square < n)
 	{
-		return (_sqrt_helper(n, mid + 1, end));
+		int sqrt = _sqrt_helper(n, mid + 1, end);
+
+		if (sqrt == -1)
+		{
+			return (mid);
+		}
+		else
+		{
+			return (sqrt);
+		}
 	}
 	else
 	{
