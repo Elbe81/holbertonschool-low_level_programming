21f236d35af61ce1f2b942e2b2e75dab0b147f57
Elbe81 <luiselberivera@gmail.com>
2023-07-17 17:18:53 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - Structure to declare differrent data
- * types and variables for dog
+ * struct dog - Structure to declare elements for a dog.
+ * @name: Declared variable for struct.
+ * @age: Declared variable for struct.
+ * @owner: Declared variable for struct.
  */
 
 struct dog
-
 {
-  char* name;
-  float age;
-  char* owner;
+char  *name;
+float age;
+char *owner;
 };
+
+#endif /* DOG_H */
