e04eb8bb141d436b111f8c99bde324e1d14973f3
Elbe81 <luiselberivera@gmail.com>
2023-08-02 23:25:50 +0000

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
-	if (index >= sizeof(unsigned long int) * 8) /* Check if index is out of range */
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	mask = 1UL << index;
