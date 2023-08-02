445618b0ae80c1c56eff591e2f093c053fc5a6b2
Elbe81 <luiselberivera@gmail.com>
2023-08-02 20:07:52 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,8 @@ dlistint_t *create_dnodeint(int n)
 }
 
 /**
- * insert_at_beginning - Insert a new node at the beginning of a doubly linked list.
+ * insert_at_beginning - Insert a new node
+ *at the beginning of a doubly linked list.
  * @h: Pointer to the pointer of the head node.
  * @new_node: Pointer to the new node to be inserted.
  */
@@ -34,9 +35,11 @@ void insert_at_beginning(dlistint_t **h, dlistint_t *new_node)
 }
 
 /**
- * insert_dnodeint_at_index - Insert a new node at a given position in a doubly linked list.
+ * insert_dnodeint_at_index - Insert a new node
+ * at a given position in a doubly linked list.
  * @h: Pointer to the pointer of the head node.
- * @idx: Index of the position where the new node should be inserted (starting from 0).
+ * @idx: Index of the position where the new node should
+ *be inserted (starting from 0).
  * @n: Integer value to be added in the new node.
  *
  * Return: Address of the new node, or NULL if it failed.
@@ -67,19 +70,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 	}
 
-	if (current == NULL)
+	if (current == NULL && count != idx)
 	{
 		/* Index is out of bounds, can't insert */
 		free(new_node);
 		return (NULL);
 	}
 
-	/* Insert in the middle or at the end */
-	new_node->prev = current->prev;
-	new_node->next = current;
-	if (current->prev != NULL)
+	if (current == NULL)
+	{
+		/* Insert at the end */
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		current = *h;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+		new_node->prev = current;
+	}
+	else
+	{
+		/* Insert in the middle */
+		new_node->prev = current->prev;
+		new_node->next = current;
 		current->prev->next = new_node;
-	current->prev = new_node;
+		current->prev = new_node;
+	}
 
 	return (new_node);
 }
