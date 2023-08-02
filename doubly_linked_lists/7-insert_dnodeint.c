ba8b1d857c2af0bce049aabf480aab623ffb649a
Elbe81 <luiselberivera@gmail.com>
2023-08-02 20:47:42 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * create_dnodeint - Creates a new node
+ *and initializes it with the given value
+ * @n: Value to be stored in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+static dlistint_t *create_dnodeint(int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * insert_dnodeint_at_index - Inserts a new node
  * at a given position in a doubly linked list
  * @h: Pointer to the pointer to the head of the list
@@ -10,25 +28,22 @@
  *
  * Return: Address of the new node, or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current_node = *h;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (!h)
 		/* Check if the pointer to the head is NULL */
 		return (NULL);
 
-	/* Create a new node and assign its value */
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-
 	if (idx == 0)
 		/* Insert at the beginning */
 	{
-		new_node->prev = NULL;
+		new_node = create_dnodeint(n);
+		if (!new_node)
+			return (NULL);
 		new_node->next = current_node;
 		if (current_node)
 			current_node->prev = new_node;
@@ -37,26 +52,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	/* Traverse the list to the position before the insertion point */
-	while (i < idx - 1)
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (!current_node)
 			/* If the list is not long enough, return NULL */
-		{
-			free(new_node);
 			return (NULL);
-		}
 		current_node = current_node->next;
-		i++;
 	}
 
 	if (!current_node)
 		/* If the list is not long enough, return NULL */
-	{
-		free(new_node);
 		return (NULL);
-	}
 
 	/* Insert the new node */
+	new_node = create_dnodeint(n);
+	if (!new_node)
+		return (NULL);
 	new_node->prev = current_node;
 	new_node->next = current_node->next;
 	if (current_node->next)
@@ -65,4 +76,3 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	return (new_node);
 }
-
