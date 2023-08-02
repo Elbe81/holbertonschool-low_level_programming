93b77c351909c5adfed2ed1bac86c4a6ca388e59
Elbe81 <luiselberivera@gmail.com>
2023-08-02 19:22:41 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,10 +13,9 @@ size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
 
-	while (h != NULL)
-
-	{
-		if (h->str == NULL)
+while (h != NULL)
+{
+if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
