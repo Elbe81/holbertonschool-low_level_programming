bb244a41ef3b3d12fea08d4cb8a7ccc1a83275b0
Elbe81 <luiselberivera@gmail.com>
2023-12-08 17:39:44 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
-
 /**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table to add or update the key/value to
@@ -21,7 +20,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
 
-	/* Check if the key already exists in the linked list at the index */
 	while (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
@@ -32,15 +30,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current_node = current_node->next;
 	}
-
-	/* Create a new node and add it to the beginning of the list in case of collision */
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
 	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
@@ -48,9 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
